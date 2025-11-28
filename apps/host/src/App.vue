6709d2cc71bfceaa3230dad3bca5d9299f9250ef
Vinicius de Liz <vinicius.deliz@gmail.com>
2025-11-28 20:26:34 -0300

    fix: app spacing

--- apps/host/src/App.vue
+++ apps/host/src/App.vue
@@ -8,12 +8,9 @@ import Layout from './components/Layout.vue'
 
 <style>
 nav {
-  padding: 1rem;
-  background: #f0f0f0;
-  margin-bottom: 1rem;
+
 }
 nav a {
-  margin-right: 1rem;
   text-decoration: none;
   color: #333;
 }
